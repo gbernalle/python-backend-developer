15fdd52200951cd54c0571c5822e061b94d07b3a
gabrielbernalle <gabrielbbmatozinhos@gmail.com>
2024-10-03 14:48:26 -0300

    Comando adicionar clientes

--- db-tester.py
+++ db-tester.py
@@ -6,6 +6,13 @@ ROOT_PATH = Path(__file__).parent
 con = sqlite3.connect(ROOT_PATH / "primeiro_banco.db")
 cursor = con.cursor()
 
-cursor.execute(
-  "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR (100),email VARCHAR (150))"
-)
+# cursor.execute(
+#   "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR (100),email VARCHAR (150))"
+# )
+# Já está criado então não tem porque ficar disponível
+
+data =  ("Guilherme","gui@gmail.com")
+
+#Se torcar valor de data e rodar de novo, irá gerar um novo registro no bd
+cursor.execute('INSERT INTO clientes (nome, email) VALUES (?,?);', data) 
+con.commit()
