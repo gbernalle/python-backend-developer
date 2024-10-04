a2ab6c1e169dce45b5394e0d22bad465b25d3aab
gabrielbernalle <gabrielbbmatozinhos@gmail.com>
2024-10-04 12:15:05 -0300

    Incluindo metodo de remoção de dados de linha no bd

--- db-tester.py
+++ db-tester.py
@@ -6,21 +6,35 @@ ROOT_PATH = Path(__file__).parent
 con = sqlite3.connect(ROOT_PATH / "primeiro_banco.db")
 cursor = con.cursor()
 
-def criar_tabela_bd(con,cursor):
-  cursor.execute(
-    "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR (100),email VARCHAR (150))"
-  )
-  con.commit()
 
-def inserir_registro(con,cursor, nome, email):
-  data =  (nome, email)
-  #Se trocar valor de data e rodar de novo, irá gerar um novo registro no bd
-  cursor.execute('INSERT INTO clientes (nome, email) VALUES (?,?);', data) 
-  con.commit()
+def criar_tabela_bd(con, cursor):
+    cursor.execute(
+        "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR (100),email VARCHAR (150))"
+    )
+    con.commit()
+
+
+def inserir_registro(con, cursor, nome, email):
+    data = (nome, email)
+    # Se trocar valor de data e rodar de novo, irá gerar um novo registro no bd
+    cursor.execute("INSERT INTO clientes (nome, email) VALUES (?,?);", data)
+    con.commit()
+
 
 def atualizar_registro(con, cursor, nome, email, id):
-  data = (nome, email, id)
-  cursor.execute('UPDATE clientes SET nome=?, email=? WHERE id=?',data) #No WHERE geralmente vai chave primária
-  con.commit()
+    data = (nome, email, id)
+    cursor.execute(
+        "UPDATE clientes SET nome=?, email=? WHERE id=?", data
+    )  # No WHERE geralmente vai chave primária
+    con.commit()
 
-atualizar_registro(con,cursor,"Guilherme Novo", "gui@novo.com", 1)
+
+def excluir_registro(con, cursor, id):
+    data = (id,)
+    cursor.execute("DELETE FROM clientes WHERE id=?", data)
+    con.commit()
+
+
+# inserir_registro(con, cursor, "Tester02", "teste@novo.com")
+# atualizar_registro(con,cursor,"Guilherme Novo", "gui@novo.com", 1)
+# excluir_registro(con, cursor, 3)
